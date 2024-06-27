e28e029bb325028458ccf00dba36db46dd8b378f
Oge Nnadi <oge@nnadi.org>
2024-06-27 10:07:31 -0500

    add min coin change
    from loiane groner's JS book

---
+++ min-coin-change-dp.js
@@ -0,0 +1,31 @@
+function minCoinChange(coins, amount) {
+    const cache = [];
+
+    const makeChange = (value) => {
+        if (!value) return [];
+        if (cache[value]) return cache[value];
+
+        let min = [];
+        let newMin;
+        let newAmount;
+
+        for (const coin of coins){
+            newAmount = value - coin;
+
+            if(newAmount >= 0) {
+                newMin = makeChange(newAmount);
+            }
+
+            if ( (newMin.length < min.length - 1) || !min.length ){
+                min = [coin].concat(newMin)
+            }
+        }
+
+        cache[value] = min;
+        return min;
+    }
+
+    console.log( makeChange(amount) );
+}
+
+minCoinChange([1 ,6 ,10, 25], 37)
