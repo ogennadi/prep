434c7e1b99e0989110eab9462e85f75b65ab4e05
Oge Nnadi <oge@nnadi.org>
2024-06-28 11:55:32 -0500

    add count-bits-set

---
+++ count-bits-set-1-to-n.js
@@ -0,0 +1,13 @@
+function totalSetBitsFrom1ToN(n) {
+    let totalSetBitCount = 0;
+    for (let number = 1; number <= n; number++) {
+        totalSetBitCount += number.toString(2).split('1').length - 1;
+    }
+    return totalSetBitCount;
+}
+
+console.log(totalSetBitsFrom1ToN(4));
+console.log(totalSetBitsFrom1ToN(17));
+console.log(totalSetBitsFrom1ToN(30));
+
+// https://www.geeksforgeeks.org/count-total-set-bits-in-all-numbers-from-1-to-n/
